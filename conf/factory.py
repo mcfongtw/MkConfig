c343ae6e08f294a709d3109cfd25dc63040c9018
Michael Fong <mcfong.open@gmail.com>
2017-01-29 09:55:15 +0800

    * bad-indentation (W0311) (MKCONFIG-69) * Fix the identation

--- conf/factory.py
+++ conf/factory.py
@@ -11,12 +11,12 @@ class ConfigTemplateFactory(object):
     """
     @staticmethod
     def get_config_tempalte(type) :
-           default_config_template = 'collectd_jmx.template'
+        default_config_template = 'collectd_jmx.template'
 
-           if type is None:
-                   return default_config_template
-           if type == 'collectd_jmx':
-                   return 'collectd_jmx.template'
-           else:
-                   #default
-                   return default_config_template
+        if type is None:
+            return default_config_template
+        if type == 'collectd_jmx':
+            return 'collectd_jmx.template'
+        else:
+            #default
+            return default_config_template
